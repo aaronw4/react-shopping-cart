c91ef97bf0d35db193a6195781e401ee154ff361
Aaron Williams <aaronrw4@yahoo.com>
2021-04-16 01:57:56 -0700

    Updates

---
+++ precache-manifest.c863c004df31bc6b1a057b3fa2f6b16d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303561f1e9b4f50ec66ba76d8d368924",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "08e1049559d41f2c47cd",
+    "url": "/react-shopping-cart/static/css/main.645a27e3.chunk.css"
+  },
+  {
+    "revision": "83fe00d320c6751e57ee",
+    "url": "/react-shopping-cart/static/js/2.71339802.chunk.js"
+  },
+  {
+    "revision": "08e1049559d41f2c47cd",
+    "url": "/react-shopping-cart/static/js/main.01f6ec99.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
